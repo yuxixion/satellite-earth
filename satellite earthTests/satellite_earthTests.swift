d8a66d1c9eca768702502f8aae5d6198205e2e04
yuxixion <yuxi.developer@gmail.com>
2015-05-12 17:15:38 -0400

    Initial Commit

---
+++ satellite earthTests/satellite_earthTests.swift
@@ -0,0 +1,36 @@
+//
+//  satellite_earthTests.swift
+//  satellite earthTests
+//
+//  Created by yuxi xiong on 5/12/15.
+//  Copyright (c) 2015 yuxi xiong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class satellite_earthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
